8c4fdf2468eb133f46cf016b6b662c2379c9d54a
Russell <RussellGilmore@Live.com>
2017-10-29 17:31:37 -0400

    Added listener to close screen saver. Added fullscreen functionality.

--- main.js
+++ main.js
@@ -4,29 +4,26 @@ const {
 } = require('electron');
 const path = require('path');
 const url = require('url');
-//init window
+
 let win;
 
 function createWindow() {
   //Window
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
     icon: __dirname + '/img/target.png'
   });
+  win.maximize();
+  win.setFullScreen(true)
   // Fetch view
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }));
-
-  // Open Devtools
-  //win.webContents.openDevTools();
-  // lamda function
   win.on('closed', () => {
     win = null;
   });
+  win.setMenu(null);
 
 }
 
